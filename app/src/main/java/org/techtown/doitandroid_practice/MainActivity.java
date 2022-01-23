0e1dd08cd473dc4920c32d154ff6714cea5341a0
Jong-Hyuk.Lim <ljohny1987@gmail.com>
2022-01-23 23:28:54 +0100

    First_Practice: Add 3 Buttons and 3 Actions(Toast Message, Open a Website, Call)

--- app/src/main/java/org/techtown/doitandroid_practice/MainActivity.java
+++ app/src/main/java/org/techtown/doitandroid_practice/MainActivity.java
@@ -1,8 +1,12 @@
 package org.techtown.doitandroid_practice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,22 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View view)
+    {
+        Toast.makeText(this, "확인1 버튼이 눌렸어요", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButton2Clicked(View view)
+    {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View view)
+    {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
     }
 }

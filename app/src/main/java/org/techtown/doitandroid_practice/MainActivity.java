f0be8ffbf3671fe405d8688cc2190985b3aef9f1
Jong-Hyuk.Lim <ljohny1987@gmail.com>
2022-01-27 20:13:52 +0100

    framelayout_practice

--- app/src/main/java/org/techtown/doitandroid_practice/MainActivity.java
+++ app/src/main/java/org/techtown/doitandroid_practice/MainActivity.java
@@ -1,17 +1,48 @@
 package org.techtown.doitandroid_practice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView imageView;
+    ImageView imageView2;
+
+    int imageIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_samplelinearlayout);
+        setContentView(R.layout.activity_framelayout);
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
     }
 
+    public void onButton33Clicked(View view)
+    {
+        changeImage();
+    }
+
+    private void changeImage()
+    {
+        if(imageIndex ==0)
+        {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex = 1;
+        }
+        else if (imageIndex ==1)
+        {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+
+            imageIndex = 0;
+        }
+    }
 }

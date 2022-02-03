a06774c93e9bbde76bfba80d5823894dab29c122
Jong-Hyuk.Lim <ljohny1987@gmail.com>
2022-02-03 22:45:29 +0100

    'iamge'

---
+++ app/src/main/java/org/techtown/doitandroid_practice/ImageActivity.java
@@ -0,0 +1,16 @@
+package org.techtown.doitandroid_practice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image);
+    }
+
+}

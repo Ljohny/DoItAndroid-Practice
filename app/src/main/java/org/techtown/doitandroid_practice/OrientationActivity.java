8abfb6423cf0d4725f0c1d99a4065fd5930194fe
Jong-Hyuk.Lim <ljohny1987@gmail.com>
2022-02-10 21:17:44 +0100

    Orientation_02

--- app/src/main/java/org/techtown/doitandroid_practice/OrientationActivity.java
+++ app/src/main/java/org/techtown/doitandroid_practice/OrientationActivity.java
@@ -1,18 +1,50 @@
 package org.techtown.doitandroid_practice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrientationActivity extends AppCompatActivity
 {
+    String name;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orientation);
         showToast("onString 호출됨.");
+
+        editText=findViewById(R.id.editTextTextPersonName3);
+
+        Button button = findViewById(R.id.button36);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                name = editText.getText().toString();
+                showToast("입력된 값을 변수에 저장했습니다 : " + name);
+            }
+        });
+
+        if(savedInstanceState !=null)
+        {
+            name = savedInstanceState.getString("name");
+            showToast("값을 복원했습니다. : " + name);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name",name);
     }
 
     @Override
